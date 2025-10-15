988fceb763d54edfc55b141c0927ddb094f36990
julian-velez <60607869+julian-velez@users.noreply.github.com>
2025-10-15 07:50:06 -0500

    if_python
    if_python

---
+++ IF/maim.py
@@ -0,0 +1,8 @@
+# Se asigna el valor 20 a la variable llamada 'numero'
+numero = 20  
+
+# Se utiliza una estructura condicional 'if' para comprobar si el valor de 'numero' es mayor que 10
+if numero > 10:  
+    # Si la condición anterior se cumple (es decir, si 'numero' es mayor que 10),
+    # entonces se ejecuta esta línea, que muestra un mensaje por pantalla
+    print("el numero es mayor que 10")  
